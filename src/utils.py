47000c25ec71e587159350554cd46638e3a52ed8
ztstat <zijtian0418@gmail.com>
2026-02-05 14:58:00 -0800

    refactor: unify logging via shared Logger utility

---
+++ src/utils.py
@@ -0,0 +1,24 @@
+# src/utils.py
+from dataclasses import dataclass
+from typing import TextIO
+import os
+
+
+@dataclass
+class Logger:
+    file: TextIO
+
+    def log(self, msg: str) -> None:
+        print(msg)
+        self.file.write(msg + "\n")
+        self.file.flush()
+
+
+def ensure_dir(path: str) -> None:
+    os.makedirs(path, exist_ok=True)
+
+
+def open_logger(results_dir: str, filename: str = "run.log") -> Logger:
+    ensure_dir(results_dir)
+    f = open(os.path.join(results_dir, filename), "w", encoding="utf-8")
+    return Logger(file=f)
